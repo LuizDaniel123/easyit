77296cb3e7918384cabfcf6ed890082f6f6e10c6
LuizDaniel123 <luizespindola275@gmail.com>
2023-03-27 15:23:14 -0500

    feat: Chama o componente Information

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import { React } from "react";
 import Header from "./Components/Header";
+import Information from "./Components/Information";
 
 const App = () => {
     return (
-        <Header/>
+        <>
+            <Header/>
+            <Information/>
+            
+        </>
     )
 }
 
