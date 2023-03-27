281c3df5257cc1bf5efe6eba0c8e481710649238
LuizDaniel123 <luizespindola275@gmail.com>
2023-03-27 12:40:19 -0500

    fix: Alteração dos nomes das tags

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,19 +5,19 @@ import './style.css'
 export default function Header() {
     return (
 
-        <header className='header'>
+        <header id='header'>
 
-            <h1 className='header_titulo'>EASYIT</h1>
+            <h1 id='header_titulo'>EASYIT</h1>
 
-            <nav className='nav'>
+            <nav id='nav'>
                 <a className='nav_links' href='#'>Onde comprar</a>
                 <a className='nav_links' href='#'>Para varejistas</a>
                 <a className='nav_links' href='#'>Ajuda</a>
             </nav>
 
-            <div className='buttons'>
-                <button className='buttons_entrar'>ENTRAR</button>
-                <button className='buttons_criar-conta'>CRIAR CONTA</button>
+            <div id='button-container'>
+                <button id='button-container_entrar'>ENTRAR</button>
+                <button id='button-container_criar-conta'>CRIAR CONTA</button>
             </div>
 
         </header>
